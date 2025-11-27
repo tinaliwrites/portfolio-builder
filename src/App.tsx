d10e04d76b75e0f6dc0a8d88538282adcb67f2cf
minioncode <tinany721@gmail.com>
2025-11-27 01:32:37 -0500

    Fix routing: Add basename to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio-builder">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/writing" element={<Writing />} />
